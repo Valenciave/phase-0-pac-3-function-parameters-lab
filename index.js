0589d055152f2dd6ad893e0dc8d516c7aa795754
Fernando Valencia <fvalencia675@gmail.com>
2023-06-10 17:14:35 -0700

    Parameter

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(value1 = "Aki") {
+    return(`Hi, my name is ${value1}.`);
+  }
+  
+  function introductionWithLanguage(name, language) {
+     return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguageOptional(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguageOptional(name , language = "JavaScript") {
+     return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
